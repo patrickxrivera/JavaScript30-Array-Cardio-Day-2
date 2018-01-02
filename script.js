c78b956fee61023a750d820d5f8b0184a6e17899
Patrick <patrick.x.rivera@gmail.com>
2018-01-02 14:18:59 -0500

    Initialized script file

--- script.js
+++ script.js
@@ -17,6 +17,10 @@ const comments = [
 
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
+const some = (people) => {
+  
+}
+
 // Array.prototype.every() // is everyone 19 or older?
 
 // Array.prototype.find()
